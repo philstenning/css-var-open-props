1217aa28a75bce2b761c4a00ba81c8fb08b3789d
Phil Stenning <philstenning@hotmail.com>
2022-07-14 11:46:10 +0100

    postcss.json error on linux so removed for now

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-const OpenProps = require('open-props')
-const postcssImport = require('postcss-import')
-const postcssNesting = require('postcss-nesting')
-const postcssJitProps = require('postcss-jit-props')
-
-module.exports = {
-  plugins: [postcssImport(), postcssJitProps(OpenProps), postcssNesting()]
-}
